211e6071811a18720bb63f182883e1e56ed24e3d
Bandish04 <71032603+Bandish04@users.noreply.github.com>
2025-03-22 13:08:47 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api", require("./routes/vitalsRoutes"));
 app.use("/api/checkin", require("./routes/checkInRoutes"));
 app.use("/api/vitals", require("./routes/vitalsRoutes"));
+app.use("/api/prescriptions", require("./routes/prescriptionRoutes"));
+require("./utils/medicationTest"); // Runs the test reminder
+//require("./utils/medicationScheduler"); // Start the medication reminder scheduler
+
 
 // Start the server
 const PORT = process.env.PORT || 5000;
