4017fc16508a8148b368c4b5fd578d82a40146b3
dev-diamondboi <ovovwero.unuavwodo@icloud.com>
2025-04-11 07:09:15 -0400

    fix: update authentication endpoint paths in LoginPage component

--- Backend/jobs/checkMissedMedications.js
+++ Backend/jobs/checkMissedMedications.js
@@ -5,22 +5,22 @@ const User = require("../models/User");
 const checkMissedMedications = async () => {
   const now = new Date();
 
-  // Find medications that were scheduled over 1 hour ago but not taken
   const missedMeds = await Prescription.find({
     taken: false,
     time: { $lte: new Date(now - 60 * 60000) } // 1 hour past due
   }).populate("userId doctorId");
 
   for (const med of missedMeds) {
-    const caregiver = await User.findOne({ role: "caregiver", _id: med.userId.assignedCaregiver });
+    const elderly = med.userId;
+    const caregiverId = elderly?.assignedCaregiver;
 
-    if (caregiver) {
+    if (caregiverId) {
       await Notification.create({
-        user: caregiver._id,
-        message: `🚨 ${med.userId.name} missed their ${med.medicationName}! Check on them.`
+        userId: caregiverId,
+        message: `🚨 ${elderly.name} missed their ${med.medicationName}! Check on them.`,
+        type: "medication",
+        read: false
       });
-
-     
     }
   }
 };
